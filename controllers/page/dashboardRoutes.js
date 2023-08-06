8734230d6e19fb1115fc884c3aa7d16b854d709a
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-08-05 20:21:56 -0700

    GET /post/:id route

--- controllers/page/dashboardRoutes.js
+++ controllers/page/dashboardRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const { auth } = require('../../utils');
 
-
+router.get('/', auth, (req, res) => {
+    res.render('dashboard');
+});
 
 module.exports = router;
--- controllers/page/postRoutes.js
+++ controllers/page/postRoutes.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
+const Post = require('../../models/Post');
+
+router.get('/:id', async (req, res) => {
+    const post = (await Post.findOne({ where: { id: req.params.id } })).get({ plain: true });
+
+    res.render('post', post);
+});
 
 module.exports = router;
